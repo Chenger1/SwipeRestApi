6c88268a220cfb7f1c5899bb003c2833304427cc
Chenger1 <exs2199@gmail.com>
2021-07-08 21:27:24 +0300

    Add permission

--- swipe/main/views/user_views.py
+++ swipe/main/views/user_views.py
@@ -2,17 +2,17 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
 from main.serializers import UserSerializer
-
-from rest_framework.viewsets import ModelViewSet
+from main.permissions import IsProfileOwner
 
 
 User = get_user_model()
 
 
 class UserViewSet(ModelViewSet):
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated, IsProfileOwner)
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
