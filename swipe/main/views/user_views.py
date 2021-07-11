ef2c953b53f4f9aa736d1cf7548c3291eb926618
Chenger1 <exs2199@gmail.com>
2021-07-11 21:23:20 +0300

    Fix

--- swipe/main/views/user_views.py
+++ swipe/main/views/user_views.py
@@ -6,7 +6,6 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.parsers import FileUploadParser
 
 from main import serializers
 from main.permissions import IsProfileOwner, IsOwner
