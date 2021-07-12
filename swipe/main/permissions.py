dcfbb4191d3bb8a7570194ce7f74705faa3bc6fc
Chenger1 <exs2199@gmail.com>
2021-07-12 21:54:45 +0300

    Update permissions

--- swipe/main/permissions.py
+++ swipe/main/permissions.py
@@ -14,14 +14,18 @@ class IsProfileOwner(BasePermission):
 
 class IsOwner(BasePermission):
     """
-    Checks permissions for user profile.
     User can get access only for his personal info.
     Trying to get another user info will be restricted
     """
     def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
         if hasattr(obj, 'sender') and hasattr(obj, 'receiver'):
             #  This condition checks permissions to get list of messages
             return bool(obj.sender == request.user or obj.receiver == request.user)
-        if request.user.is_staff:
-            return True
-        return obj.user.uid == request.user.uid
+        elif hasattr(obj, 'user'):
+            return obj.user.uid == request.user.uid
+        elif hasattr(obj, 'sales_department'):
+            return obj.sales_department.uid == request.user.uid
+        else:
+            return False
