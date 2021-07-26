617d326b476ca3091e774bf30c281d3a6aa3896a
Chenger1 <exs2199@gmail.com>
2021-07-26 23:22:40 +0300

    Fix

--- swipe/_db/models/manager.py
+++ swipe/_db/models/manager.py
@@ -3,9 +3,8 @@ from django.contrib.auth.base_user import BaseUserManager
 
 class UserManager(BaseUserManager):
     def create_user(self, email='', password='', **extra_fields):
-        if not email:
-            raise ValueError('Email не заполнен')
-        email = self.normalize_email(email)
+        if email:
+            email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
